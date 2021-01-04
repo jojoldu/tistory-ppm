4f6fffb93d31707528c2c736bd2e12541fe407a6
이동욱 <jojoldu@gmail.com>
2021-01-04 11:35:34 +0900

    토큰 테스트 없이 하기 위해 jest mock 추가

--- src/js/lib.test.js
+++ src/js/lib.test.js
@@ -1,7 +1,71 @@
 import {
     transform,
     groupBy,
+    getPageParameters,
+    getTotalCount,
+    getPosts
 } from "./lib";
+
+const axios = require("axios")
+
+describe('페이지 수가 배열에 담긴다.', () => {
+    it('전체 글이 38개면 [1,2,3,4]가 반환된다', () => {
+        const result = getPageParameters(38);
+
+        expect(result).toStrictEqual([1,2,3,4]);
+    });
+
+    it('전체 글이 40개면 [1,2,3,4]가 반환된다', () => {
+        const result = getPageParameters(40);
+
+        expect(result).toStrictEqual([1,2,3,4]);
+    });
+
+    it('전체 글이 41개면 [1,2,3,4,5]가 반환된다', () => {
+        const result = getPageParameters(41);
+
+        expect(result).toStrictEqual([1,2,3,4,5]);
+    });
+});
+
+test('tistory의 전체 글 수를 가져온다', async () => {
+    const totalCount = 181;
+    axios.get = jest.fn().mockResolvedValue({
+        data: {
+            "tistory": {
+                "status": "200",
+                "item": {
+                    "totalCount": totalCount
+                }
+            }
+        }
+    })
+
+    const result = await getTotalCount("testToken", "jojoldu", 1);
+
+    expect(result).toBe(totalCount);
+});
+
+test('tistory의 첫번째 페이지 목록을 가져온다', async () => {
+    axios.get = jest.fn().mockResolvedValue({
+        data: {
+            "tistory": {
+                "status": "200",
+                "item": {
+                    "posts": [
+                        {
+                            "date": "2018-06-01 17:54:28"
+                        }
+                    ]
+                }
+            }
+        }
+    })
+
+    const result = await getPosts("testToken", "jojoldu", 1);
+
+    expect(result.length).toBe(1);
+});
 
 test('일시를 yyyy-MM으로 변환한다.',  () => {
     const result = transform("2021-01-01 19:02:10");
